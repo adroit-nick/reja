d38622572c114791272b84b7b25ff3100456244a
admin <abrortoshaliyev@gmail.com>
2024-11-13 21:19:13 +0900

    yangi fayl qo'shildi

--- server.js
+++ server.js
@@ -9,19 +9,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2 Session code
-
 // 3 views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 
 // 4  routing code
+app.get("/", function (req, res) {
+    res.render("harid");
+
+});
+
+
 app.get("/hello", function(req, res) {  
     res.end("<h1>Hello World by me</h1>");
 });
-app.get("/gift", function(req, res) {  
-    res.end("<h1>Siz sovg'alar sahifasidasiz</h1>");
+app.get("/list", function(req, res) {  
+    res.end("banan, olcha, olma");
 });
+app.get("/", function(req, res)   {
+     res.render('comment yaratish eski.ejs');
+ });
 
 
 const server = http.createServer(app);
