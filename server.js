7048674e4018b8f738d396e30f18cfa8615219a0
admin <abrortoshaliyev@gmail.com>
2024-11-17 12:56:00 +0900

    added new files

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
+console.log("Hello world!");
+const express = require ("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 
 let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    }   else {
-        user = JSON.parse(data)
-    }
+fs.readFile("database/user.json", "utf8" , (err,data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
 });
 
-// 1 Kirish code
+//
+// 1 kirish codelari
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// 2 Session code
-// 3 views code
+
+// 2 Session ga bogliq kodlar
+
+// 3 views ga bogliq codelar
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+// 4 routingga bog'liq codelar
 
-// 4  routing code
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test:"successfully"});
-})
-
-app.get("/author", function (req, res) {
-    res.render("author");
+app.post("/create-item", (req, res)=>{
+   console.log(req.body);
+   res.json({test: "succes"});
 });
 
-app.get("/", function (req, res) {
-    res.render("harid");
+// publishingdagi codelar
+
+app.get('/author', (req, res) =>{
+  res.render("author", {user: user});
 });
 
+
+
+app.get("/", function(req, res){
+  res.render("harid");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT , function () {
+  console.log(`The server is running sucsessfully on port: ${PORT}`);
+
+});
 
 // app.get("/hello", function(req, res) {  
 //     res.end("<h1>Hello World by me</h1>");
@@ -49,9 +63,3 @@ app.get("/", function (req, res) {
 //      res.render('harid.ejs');
 //  });
 
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
-});
