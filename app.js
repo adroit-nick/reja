6ebb3afd12453c2fc2f7a7c4955e933b72ef7964
admin <abrortoshaliyev@gmail.com>
2024-11-27 16:05:40 +0900

    feat: mongoDB ulanish logic

--- app.js
+++ app.js
@@ -1,61 +1,34 @@
 console.log("Hello world!");
-const e = require("express");
 const express = require ("express");
+const res = require("express/lib/response");
 const app = express();
-const fs = require("fs");
 
 // mongo db ni chaqirish
-
 const db = require("./server").db();
-const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8" , (err,data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
-
-//MongoDBni ulaymiz
-
-
-
-// 1 kirish codelari
+//1:Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-// 2 Session ga bogliq kodlar
-
-// 3 views ga bogliq codelar
+//2:Session code
+//3:View code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4 routingga bog'liq codelar
-
+//4:Routing code
 app.post("/create-item", (req, res)=>{
-  console.log("user entered /create-item");
   console.log(req.body);
   const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja}, (err,data)=>{
-    if(err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else{
-      res.end("successfully added");
-    }
- });
+db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+  if(err) {
+    console.log(err);
+    res.end('something went wrong');
+  } else {
+    res.end('successfully added');
+  }
+  });
 });
-
-// publishingdagi codelar
-
-app.get('/author', (req, res) =>{
-  res.render("author", {user: user});
-});
-
 
 
 app.get("/", function(req, res){
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 const http = require("http");
 const mongodb = require("mongodb");
 
-// MongoDB ulash
 let db;
-
 const connectionString =
   "mongodb+srv://AbrorT:abrormongodb94@cluster0.ct5ep.mongodb.net/";
 
-// MongoDB ulanish
 mongodb.connect(
   connectionString,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }, (err, client) => {
-    if (err) { console.log("Error on Connection MongoDB");
-    } else {
+  }, 
+  (err, client) => {
+    if (err)  console.log("Error on Connection MongoDB");
+     else {
       console.log("MongoDB connection succeed");
       module.exports = client;
-
 
       const app = require("./app");
       const server = http.createServer(app);
