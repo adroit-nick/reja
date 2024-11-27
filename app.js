1722f0b372fd3d31dd94859a8dc00a9ce7734671
admin <abrortoshaliyev@gmail.com>
2024-11-27 18:15:26 +0900

    feat: FrontEnd jasascriptni qurish

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express();
 
 // mongo db ni chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
+
+let user;
+// fs.readFile
+("database/user.json", "utf8", (err, data) => 
+  {
+  if(err) {
+    console.log("ERROR:", err);
+  } else { user = JSON.parse(data)
+ }
+});
 
 //1:Kirish code
 app.use(express.static("public"));
@@ -17,23 +28,33 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4:Routing code
-app.post("/create-item", (req, res)=>{
+app.post("/create-item", (req, res)=> {
+  console.log("user entered /create-item");
   console.log(req.body);
   const new_reja = req.body.reja;
-db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-  if(err) {
-    console.log(err);
-    res.end('something went wrong');
-  } else {
-    res.end('successfully added');
-  }
+  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
-});
+  });
+
+  app.post("/delete-item", (req, res) =>{
+    const id = req.body.id;
+   db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data){
+     res.json({state: "success"})});
+   });
+
+
+   app.get('/author', (req, res) =>{
+    res.render("author", {user: user});
+  });
 
 
 app.get("/", function(req, res){
   console.log("user entered /");
-  db.collection("plans").find().toArray( (err, data) => {
+  db.collection("plans")
+  .find()
+  .toArray( (err, data) => {
     if(err) {
       console.log(err);
       res.end("something went wrong");
